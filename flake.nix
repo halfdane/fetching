e441343bbf04e1a72b9bab07dc46e901672d072a
halfdane <halfdane@gmx.net>
2026-02-20 15:15:20 +0100

    be able to build the binary for both arch64 and x86

--- flake.nix
+++ flake.nix
@@ -26,10 +26,33 @@
           export RUST_BACKTRACE=1
         '';
       };
+      mkPackage = system: let
+        pkgs = import nixpkgs { inherit system; overlays = [ rust-overlay.overlays.default ]; };
+      in pkgs.rustPlatform.buildRustPackage {
+        pname = "fetching";
+        version = "0.1.0";
+        src = ./.;
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+        nativeBuildInputs = [ pkgs.pkg-config pkgs.cmake ];
+        buildInputs = [
+          pkgs.openssl
+          pkgs.alsa-lib
+          pkgs.avahi
+          pkgs.libpulseaudio
+          pkgs.dbus
+          pkgs.vorbis-tools
+        ];
+      };
     in {
       devShells = builtins.listToAttrs (map (system: {
         name = system;
         value = { default = mkDevShell system; };
+      }) systems);
+      packages = builtins.listToAttrs (map (system: {
+        name = system;
+        value = { default = mkPackage system; };
       }) systems);
     };
 }
