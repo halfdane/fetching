7275d60494aff4b4e4ac6fb487c58b7ff775c9eb
halfdane <halfdane@gmx.net>
2026-03-09 09:23:05 +0100

    chore: bump version to v0.1.16

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       in
       {
         packages.default =
-          let fetchingVersion = "0.1.15"; in
+          let fetchingVersion = "0.1.16"; in
           pkgs.buildGoModule {
             pname = "fetching";
             version = fetchingVersion;
